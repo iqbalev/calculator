8617d07bc871ba63ef9a694a2a4794fcf3a3e4a9
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-07-17 17:02:28 +0700

    Added calculation functionality

--- script.js
+++ script.js
@@ -1,3 +1,50 @@
+const display = document.getElementById("display");
+const numberButtons = document.querySelectorAll(".number");
+const mathOperatorButtons = document.querySelectorAll(".operator");
+const addButton = document.getElementById("add");
+const substractButton = document.getElementById("substract");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
+const equalsButton = document.getElementById("equals");
+
+let finalResult = "";
+let selectedOperator = "";
+let firstInput = "";
+let secondInput = "";
+let isOperatorSelected = false;
+
+numberButtons.forEach((number) => {
+  number.addEventListener("click", function () {
+    if (isOperatorSelected === false) {
+      firstInput += number.textContent;
+      display.textContent = firstInput;
+    } else if (isOperatorSelected === true) {
+      secondInput += number.textContent;
+      display.textContent = secondInput;
+    }
+  });
+});
+
+mathOperatorButtons.forEach((operator) => {
+  operator.addEventListener("click", function () {
+    isOperatorSelected = true;
+    if (operator === addButton) {
+      selectedOperator = "+";
+    } else if (operator === substractButton) {
+      selectedOperator = "-";
+    } else if (operator === multiplyButton) {
+      selectedOperator = "x";
+    } else if (operator === divideButton) {
+      selectedOperator = "/";
+    }
+  });
+});
+
+equalsButton.addEventListener("click", function () {
+  calculateResult();
+  displayResult();
+});
+
 const add = (x, y) => x + y;
 const substract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
@@ -8,13 +55,13 @@ const divide = (x, y) => {
   return x / y;
 };
 
-const operate = (firstNumber, mathOperator, secondNumber) => {
+const operate = (mathOperator, firstNumber, secondNumber) => {
   switch (mathOperator) {
     case "+":
-      return add(firstNumber, secondNumber);
+      return add(firstNumber), secondNumber;
     case "-":
       return substract(firstNumber, secondNumber);
-    case "*":
+    case "x":
       return multiply(firstNumber, secondNumber);
     case "/":
       return divide(firstNumber, secondNumber);
@@ -22,3 +69,8 @@ const operate = (firstNumber, mathOperator, secondNumber) => {
       alert("You cannot do that!");
   }
 };
+
+const calculateResult = () =>
+  (finalResult = operate(selectedOperator, firstInput, secondInput));
+
+const displayResult = () => (display.textContent = finalResult);
