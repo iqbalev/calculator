f467a3006be103aecf8793e410a237ec65e25f2e
Muhammad Iqbal <ytsejam09.03.00@gmail.com>
2024-07-18 15:43:51 +0700

    Added delete functionality

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const display = document.getElementById("display");
 const clearButton = document.getElementById("clear");
+const deleteButton = document.getElementById("delete");
 const numberButtons = document.querySelectorAll(".number");
 const mathOperatorButtons = document.querySelectorAll(".operator");
 const addButton = document.getElementById("add");
@@ -34,7 +35,7 @@ mathOperatorButtons.forEach((operator) => {
     } else if (operator === subtractButton) {
       selectedOperator = "-";
     } else if (operator === multiplyButton) {
-      selectedOperator = "x";
+      selectedOperator = "*";
     } else if (operator === divideButton) {
       selectedOperator = "/";
     }
@@ -47,6 +48,8 @@ equalsButton.addEventListener("click", () => {
 });
 
 clearButton.addEventListener("click", () => clearDisplay());
+
+deleteButton.addEventListener("click", () => deleteInput());
 
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
@@ -64,7 +67,7 @@ const operate = (mathOperator, firstNumber, secondNumber) => {
       return add(firstNumber, secondNumber);
     case "-":
       return subtract(firstNumber, secondNumber);
-    case "x":
+    case "*":
       return multiply(firstNumber, secondNumber);
     case "/":
       return divide(firstNumber, secondNumber);
@@ -83,6 +86,17 @@ const clearDisplay = () => {
   selectedOperator = "";
   firstInput = "";
   secondInput = "";
+  isOperatorSelected = false;
 
   display.textContent = 0;
+};
+
+const deleteInput = () => {
+  if (isOperatorSelected === false) {
+    firstInput = firstInput.slice(0, -1);
+    display.textContent = firstInput || 0;
+  } else if (isOperatorSelected === true) {
+    secondInput = secondInput.slice(0, -1);
+    display.textContent = secondInput || 0;
+  }
 };
